b2bc193dad8679e59118a3a61be15402a790492b
Andrey Kulish <andreykulish89@gmail.com>
2023-02-22 00:46:48 +0300

    add counter time

--- src/features/Main/Content/Counter/Counter.tsx
+++ src/features/Main/Content/Counter/Counter.tsx
@@ -1,31 +1,53 @@
+import { useEffect, useState } from "react";
 import s from "./Counter.module.css";
 
 export const Counter = () => {
+  const finishDate = new Date(2023, 4, 31);
+  const [finishTime] = useState(finishDate.getTime());
+  const [[diffDays, diffH, diffM, diffS], setDiff] = useState([0, 0, 0, 0]);
+  const [tick, setTick] = useState(false);
+
+  useEffect(() => {
+    const diff = (finishTime - Number(new Date())) / 1000;
+    if (diff < 0) return; // время вышло
+    setDiff([
+      Math.floor(diff / 86400),
+      Math.floor((diff / 3600) % 24),
+      Math.floor((diff / 60) % 60),
+      Math.floor(diff % 60),
+    ]);
+  }, [tick, finishTime]);
+
+  useEffect(() => {
+    const timerID = setInterval(() => setTick(!tick), 1000);
+    return () => clearInterval(timerID);
+  }, [tick]);
+
   return (
     <div className={s.counterPage}>
       <div className={s.counter_item}>
-        <p className={s.counter_number}>92</p>
+        <p className={s.counter_number}>{diffDays}</p>
         <div className={s.counter_title}>
           <p>Days</p>
         </div>
       </div>
       <span className={s.counter_points}>:</span>
       <div className={s.counter_item}>
-        <p className={s.counter_number}>11</p>
+        <p className={s.counter_number}>{diffH.toString().padStart(2, "0")}</p>
         <div className={s.counter_title}>
           <p>Hours</p>
         </div>
       </div>
       <span className={s.counter_points}>:</span>
       <div className={s.counter_item}>
-        <p className={s.counter_number}>42</p>
+        <p className={s.counter_number}>{diffM.toString().padStart(2, "0")}</p>
         <div className={s.counter_title}>
           <p>Minutes</p>
         </div>
       </div>
       <span className={s.counter_points}>:</span>
       <div className={s.counter_item}>
-        <p className={s.counter_number}>48</p>
+        <p className={s.counter_number}>{diffS.toString().padStart(2, "0")}</p>
         <div className={s.counter_title}>
           <p>Seconds</p>
         </div>
